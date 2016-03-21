99cf920a889113f4105b8177a12b509e13aad009
Andrea Palaia <andrea@detectify.com>
2016-03-21 10:29:48 +0100

    Update CLI options
    - add  "query" option to specify a DSL query
    - add "doc_type" option to specify the elasticsearch document type
    - make "fields" option mandatory
    - handle case in which non mandatory options are not specified

--- eshits2csv.py
+++ eshits2csv.py
@@ -16,16 +16,28 @@ def get_var(input_dict, accessor_string):
 
 def query_and_dump_reults(args):
     es = Elasticsearch([args.hostname + ':' + str(args.port)])
+
     query = '{"query":{"match_all":{}}}'
+    if args.query is not None:
+        query = args.query
+
+    doc_type = None
+    if args.doc_type is not None:
+        doc_type = args.doc_type
+
+    target = "output.csv"
+    if args.target is not None:
+        target = args.target
+
     res = es.count(index=args.index, body=query)
     nhits = res['count']
 
     counter = 0
     bar = progressbar.ProgressBar(max_value=nhits)
 
-    res = helpers.scan(es, index=args.index, query=query)
+    res = helpers.scan(es, index=args.index, query=query, doc_type=doc_type)
     fields = args.fields.split(',')
-    with open(args.target, 'w') as csvfile:
+    with open(target, 'w') as csvfile:
         datawriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
         datawriter.writerow(fields)
         for item in res:
@@ -42,7 +54,9 @@ def main(argv):
     parser.add_argument("--hostname", help="ES hostname", default="localhost", type=str)
     parser.add_argument("--port", help="ES port", default="9200", type=int)
     parser.add_argument("--index", help="Name of the index", type=str)
-    parser.add_argument("--fields", help="A list of fields to extract and return", type=str)
+    parser.add_argument("--query", dest="query", help="Query DSL", type=str)
+    parser.add_argument("--doc_type", dest="doc_type", help="Document type", type=str)
+    parser.add_argument("--fields", help="A list of fields to extract and return", type=str, required=True)
     parser.add_argument("--target", help="Output csv file path", type=str)
     parser.set_defaults(func=query_and_dump_reults)
 
